c25962f5abaffbcd6b7c91384b2c8d931685c9eb
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-09-23 20:36:51 +0300

    Homework №2: Variables.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
+        // Задание 1 ()
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
